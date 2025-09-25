942fe041b5f398b63251a9cae8232e96c707f763
Angelo-roxas <aroxas@ualberta.ca>
2025-09-25 15:52:37 -0600

    commit with shape class

--- app/src/main/java/com/example/sprite/Shape.java
+++ app/src/main/java/com/example/sprite/Shape.java
@@ -1,4 +1,17 @@
 package com.example.sprite;
 
 public class Shape {
+    private int x;
+    private int y;
+
+    //add colors here?
+
+    public Shape(int x, int y){
+        x = this.x;
+        y = this.y;
+
+    }
+
+
+
 }
